9b70bb6ef936bf882159de3acee2049cec1d0e16
Adrian Mendez <mendezadrian149@gmail.com>
2020-07-14 13:21:42 -0400

    manage error in routes student

--- repository/model.go
+++ repository/model.go
@@ -3,7 +3,7 @@ package repository
 import "time"
 
 type Model struct {
-	ID        uint       `json:"id" gorm:"primary_key;AUTO_INCREMENT"`
+	ID        uint       `json:"id,omitempty" gorm:"primary_key;AUTO_INCREMENT"`
 	CreatedAt time.Time  `json:"-"`
 	UpdatedAt time.Time  `json:"-"`
 	DeletedAt *time.Time `json:"-" sql:"index"`
