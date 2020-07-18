661af11d0ac81f8b09a954fc45246eb6d51b9d13
Adrian Mendez <mendezadrian149@gmail.com>
2020-07-17 20:15:54 -0400

    adding middleware to register logs

--- handler/routes.go
+++ handler/routes.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"net/http"
 
+	"github.com/AdrianMendez1199/simple-crud-go-sql/middleware"
 	"github.com/AdrianMendez1199/simple-crud-go-sql/repository"
 	"github.com/gorilla/mux"
 )
@@ -39,15 +40,15 @@ func initServices() *API {
 }
 
 // Setting default headers like a middleware
-func setDefaultHeaders(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
-		w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, post-check=0, pre-check=0")
-		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-		next.ServeHTTP(w, r)
-	})
-}
+// func setDefaultHeaders(next http.Handler) http.Handler {
+// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		w.Header().Set("Access-Control-Allow-Origin", "*")
+// 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+// 		w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, post-check=0, pre-check=0")
+// 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+// 		next.ServeHTTP(w, r)
+// 	})
+// }
 
 // New  handdle http endpoinst
 func New() Server {
@@ -59,7 +60,7 @@ func New() Server {
 
 	// handler Studens
 	r.HandleFunc("/students", a.createStudent).Methods(http.MethodPost)
-	r.HandleFunc("/students", a.getStudents).Methods(http.MethodGet)
+	r.HandleFunc("/students", middleware.WriteLog(a.getStudents)).Methods(http.MethodGet)
 	r.HandleFunc("/student/{id}", a.getStudentByID).Methods(http.MethodGet)
 
 	// handler Teacher
@@ -72,7 +73,7 @@ func New() Server {
 	r.HandleFunc("/course/{id}", a.getCourseByID).Methods(http.MethodGet)
 	r.HandleFunc("/courses", a.getAllCourses).Methods(http.MethodGet)
 
-	r.Use(setDefaultHeaders)
+	// r.Use(setDefaultHeaders)
 	a.router = r
 	return a
 }
---
+++ middleware/middleware.go
@@ -0,0 +1,40 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
+
+// WriteLog Middleware
+func WriteLog(next func(w http.ResponseWriter, r *http.Request)) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		t := time.Now()
+
+		fmt.Println(t.Year())
+		formatted := fmt.Sprintf("%d/%d/%d",
+			t.Year(), t.Month(), t.Day(),
+		)
+
+		logPath := "logs/" + formatted
+		err := os.MkdirAll(logPath, 0755)
+		check(err)
+
+		f, err := os.OpenFile(logPath+"/mid.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		defer f.Close()
+		check(err)
+
+		_, err = f.WriteString("Hello World")
+		check(err)
+
+		next(w, r)
+	}
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
